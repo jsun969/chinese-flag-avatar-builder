5df03f79db3f75bf836e76e57e769b43503aea84
jsun969 <i@jsun969.cn>
2021-10-03 11:14:06 +0800

    chore: init frame images

---
+++ assets/frames/index.ts
@@ -0,0 +1,51 @@
+const img1 = require('./1.png');
+const img2 = require('./2.png');
+const img3 = require('./3.png');
+const img4 = require('./4.png');
+const img5 = require('./5.png');
+const img6 = require('./6.png');
+const img7 = require('./7.png');
+const img8 = require('./8.png');
+const img9 = require('./9.png');
+const img10 = require('./10.png');
+const img11 = require('./11.png');
+const img12 = require('./12.png');
+const img13 = require('./13.png');
+const img14 = require('./14.png');
+const img15 = require('./15.png');
+const img16 = require('./16.png');
+const img17 = require('./17.png');
+const img18 = require('./18.png');
+const img19 = require('./19.png');
+const img20 = require('./20.png');
+const img21 = require('./21.png');
+const img22 = require('./22.png');
+const img23 = require('./23.png');
+
+const frameImages = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+];
+
+export default frameImages;
